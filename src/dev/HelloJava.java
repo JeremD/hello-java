5b0f289fc8b5a8c9add8be6c807630b7bf0971d0
JeremD <delpech.jeremy@hotmail.fr>
2020-06-03 16:52:10 +0200

    add instruction

--- src/dev/HelloJava.java
+++ src/dev/HelloJava.java
@@ -3,7 +3,7 @@ package dev;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Ici il faut ajouter du code pour la suite
 		return;
 	}
 
